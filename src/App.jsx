39233a56515175ed4eb8e109c61a6943b5da3423
Farhaan <farhaan654@gmail.com>
2024-08-23 22:27:08 -0600

    Switched to React + Vite Framework Old Implementation is now in `Old` folder

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './App.css';
+import logoSteam from './assets/logo_steam.png';
+import placeholder from './assets/placeholder.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ProfileInput />
+      <GamePreview />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <img src={logoSteam} alt="Steam Logo" />
+    </header>
+  );
+}
+
+function ProfileInput() {
+  return (
+    <div className="profileInput">
+      <h2>Enter Your Steam Profile Link</h2>
+      <form id="profileForm">
+        <input 
+          type="text" 
+          id="profileLink" 
+          placeholder="https://steamcommunity.com/id/fr_hn/" 
+          required 
+        />
+      </form>
+      <h3>Your profile should be set to public</h3>
+    </div>
+  );
+}
+
+function GamePreview() {
+  return (
+    <div className="gamePreview" id="gamePreview">
+      <img src={placeholder} alt="Game Preview" id="gameImage" />
+      <h3 id="gameTitle">Next Game</h3>
+    </div>
+  );
+}
+
+export default App;
