e9e5fd1caf915485be47dfe74328859bfb277475
Farhaan <farhaan654@gmail.com>
2024-08-26 22:47:32 -0600

    Implemented 2 new functions: - getOwnedGames: Returns list of user's owned games - getGameDetails: Returns details about a game

--- src/steamAPI.js
+++ src/steamAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-export async function getSteamId(profileUrl) {
+async function getSteamId(profileUrl) {
 
     const urlParts = profileUrl.split('/');
     const lastPart = urlParts[urlParts.length - 1];
@@ -11,15 +11,31 @@ export async function getSteamId(profileUrl) {
       return lastPart;
     } 
     else {
-      // If it's a vanity URL, need to resolve it
-      const response = await fetch(`http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${STEAM_API_KEY}&vanityurl=${lastPart}`);
-      const data = await response.json();
+        // If it's a vanity URL, need to resolve it
+        const response = await fetch(`http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${STEAM_API_KEY}&vanityurl=${lastPart}`);
+        const data = await response.json();
 
-      if (data.response.success === 1) {
-        return data.response.steamid;
-      } 
-      else {
-        throw new Error('Failed to resolve vanity URL');
-      }
+        if (data.response.success === 1) {
+             return data.response.steamid;
+        } 
+        else {
+            throw new Error('Failed to resolve vanity URL');
+        }
     }
-  }
+}
+
+async function getOwnedGames(steamId) {
+
+    const response = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${STEAM_API_KEY}&steamid=${steamId}&format=json&include_appinfo=1`);
+    const data = await response.json();
+
+    return data.response.games;
+}
+
+async function getGameDetails(appId) {
+
+    const response = await fetch(`https://store.steampowered.com/api/appdetails?appids=${appId}`);
+    const data = await response.json();
+    
+    return data[appId].data;
+}
